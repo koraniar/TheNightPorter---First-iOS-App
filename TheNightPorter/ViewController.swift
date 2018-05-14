0d82d4aecc8f8d170243e2a640f387f94b9d6734
Koraniar  <eganfs@gmail.com>
2018-05-14 18:42:04 -0500

    Getting started module finished

--- TheNightPorter/ViewController.swift
+++ TheNightPorter/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func darkMode(_ sender: Any) {
+        view.backgroundColor = UIColor.darkGray
+    }
+    
 }
 
